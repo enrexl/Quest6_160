ae5692a730938463ffb2a747e37a2e5f80d43d04
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2024-11-22 17:21:24 +0700

    Create dataclass RuangKelas.kt

---
+++ app/src/main/java/com/example/week8_navigationmultipledata/data/RuangKelas.kt
@@ -0,0 +1,3 @@
+package com.example.week8_navigationmultipledata.data
+
+data class RuangKelas()
--- app/src/main/java/com/example/week8_navigationmultipledata/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/week8_navigationmultipledata/ui/view/screen/RencanaStudiView.kt
@@ -1,2 +1,123 @@
 package com.example.week8_navigationmultipledata.ui.view.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import com.example.week8_navigationmultipledata.model.Mahasiswa
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.week8_navigationmultipledata.R
+
+@Preview
+@Composable
+fun RencanaStudiView(
+    mahasiswa: Mahasiswa,
+    onSubmitButtonClicked:()-> Unit,
+    onBackButtonClicked:()-> Unit
+){
+    var chosenDropdown by remember { mutableStateOf("") }
+    var checked by remember { mutableStateOf(false) }
+    var pilihanKelas by remember { mutableStateOf("") }
+    var listData: MutableList<String> = mutableListOf(chosenDropdown,pilihanKelas)
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.Primary))
+    ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(painter = painterResource(id = R.drawable.logo),
+                contentDescription = "",
+                modifier = Modifier.size(150.dp)
+            )
+
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = mahasiswa.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 15.sp,
+                    color = Color.White
+                )
+                Text(
+                    text = mahasiswa.nim,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 12.sp,
+                    color = Color.White
+                )
+
+            }
+
+            Box {
+                Icon(
+                    imageVector = Icons.Filled.Notifications,
+                    contentDescription = "",
+                    tint = Color.White
+                )
+            }
+
+        }
+        Box(modifier = Modifier
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(
+                    topEnd = 20.dp,
+                    topStart = 20.dp
+                )
+            )
+            .fillMaxSize())
+        {
+            Text(text = "Pilih Mata Kuliah Peminatan",
+                fontWeight = FontWeight.Bold)
+
+            Text(text = "Silahkan pilih mata kuliah yang diinginkan",
+                fontWeight = FontWeight.Light,
+                fontSize = 12.sp)
+            Spacer(modifier = Modifier.padding(8.dp))
+
+            DynamicSelectTextField()
+
+            Spacer(modifier = Modifier.padding(8.dp))
+            Row(modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly)
+            {
+
+            }
+        }
+
+    }
+
+}

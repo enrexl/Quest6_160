7699dcb103bc1b30d5b3e9010f77a584ad8d257f
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2024-11-22 17:59:24 +0700

    Create RencanaStudi.kt

---
+++ app/src/main/java/com/example/week8_navigationmultipledata/model/RencanaStudi.kt
@@ -0,0 +1,6 @@
+package com.example.week8_navigationmultipledata.model
+
+data class RencanaStudi(
+    val namaMK: String = "",
+    val kelas: String ="",
+)
--- app/src/main/java/com/example/week8_navigationmultipledata/ui/view/viewmodel/RencanaStudiViewModel.kt
+++ app/src/main/java/com/example/week8_navigationmultipledata/ui/view/viewmodel/RencanaStudiViewModel.kt
@@ -1,4 +1,9 @@
 package com.example.week8_navigationmultipledata.ui.view.viewmodel
 
-class RencanaStudiViewModel {
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class RencanaStudiViewModel : ViewModel() {
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsSta
 }
